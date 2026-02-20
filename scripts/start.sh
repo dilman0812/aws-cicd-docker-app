62bf95cbcc1807547252b57eea393241fa50bee4
Dilman Sandhu <dilman3349@gmail.com>
2026-02-20 15:20:19 +0530

    Phase 5: add ECR authentication inside deployment lifecycle

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-IMAGE="020423264252.dkr.ecr.ap-south-1.amazonaws.com/aws-cicd-webapp:latest"
+AWS_REGION="ap-south-1"
+ACCOUNT_ID="020423264252"
+REPO_NAME="aws-cicd-webapp"
+
+IMAGE="$ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:latest"
+
+echo "Logging into Amazon ECR..."
+aws ecr get-login-password --region $AWS_REGION | \
+docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+
+echo "Stopping old container..."
+docker stop aws-cicd-container || true
+docker rm aws-cicd-container || true
 
 echo "Pulling latest image..."
 docker pull $IMAGE
 
-echo "Stopping existing container..."
-docker stop aws-cicd-container || true
-docker rm aws-cicd-container || true
-
-echo "Removing old image..."
-docker image prune -af
-
 echo "Starting new container..."
-docker run -d --pull always -p 80:5000 --name aws-cicd-container $IMAGE
+docker run -d -p 80:5000 --name aws-cicd-container $IMAGE
