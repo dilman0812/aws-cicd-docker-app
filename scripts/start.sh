eaa8d3f57dfff898cac1c29c0e273eeb72c08164
Dilman Sandhu <dilman3349@gmail.com>
2026-02-20 14:54:42 +0530

    Phase 5: force fresh Docker image pull during deployment

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-docker pull 020423264252.dkr.ecr.ap-south-1.amazonaws.com/aws-cicd-webapp:latest
-docker run -d -p 80:5000 --name aws-cicd-container \
-020423264252.dkr.ecr.ap-south-1.amazonaws.com/aws-cicd-webapp:latest
 
+IMAGE="020423264252.dkr.ecr.ap-south-1.amazonaws.com/aws-cicd-webapp:latest"
+
+echo "Pulling latest image..."
+docker pull $IMAGE
+
+echo "Stopping existing container..."
+docker stop aws-cicd-container || true
+docker rm aws-cicd-container || true
+
+echo "Removing old image..."
+docker image prune -af
+
+echo "Starting new container..."
+docker run -d --pull always -p 80:5000 --name aws-cicd-container $IMAGE
